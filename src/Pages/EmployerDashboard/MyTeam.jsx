a4bdb5bd5926cdc2f3743852f7b109e5b8fa6fd0
AbbasWork7 <syedabbas.keykoders@gmail.com>
2025-06-18 15:16:08 +0530

    Delete src/Pages/EmployerDashboard directory

--- src/Pages/EmployerDashboard/MyTeam.jsx
+++
@@ -1,8 +0,0 @@
-export default function MyTeam() {
-  return (
-    <div className="animate-fadeIn px-4 mt-6 md:mt-0 md:px-0">
-      <h2 className="text-3xl font-bold mb-4 md:ml-0 ml-10">👥 My Team</h2>
-      <p className="md:ml-0 ml-10">View and manage your employees here.</p>
-    </div>
-  );
-}
--- src/Pages/EmployerDashboard/Notification.jsx
+++
@@ -1,8 +0,0 @@
-export default function Notification() {
-  return (
-    <div className="animate-fadeIn">
-      <h2 className="text-3xl font-bold mb-4">🔔 Notifications</h2>
-      <p>Stay updated with your latest notifications.</p>
-    </div>
-  );
-}
