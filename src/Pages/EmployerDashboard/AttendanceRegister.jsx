af247d8729ceda95aab4a8e8b7811efde7897897
AbbasWork7 <syedabbas.keykoders@gmail.com>
2025-06-18 11:40:08 +0530

    Create AttendanceRegister.jsx

---
+++ src/Pages/EmployerDashboard/AttendanceRegister.jsx
@@ -0,0 +1,8 @@
+export default function AttemdanceRegister() {
+  return (
+    <div className="text-center animate-fadeIn">
+      <h1 className="text-4xl font-bold mb-4">🏠 Welcome to your Dashboard</h1>
+      <p className="text-lg">Track your team's attendance, performance, and more!</p>
+    </div>
+  );
+}
