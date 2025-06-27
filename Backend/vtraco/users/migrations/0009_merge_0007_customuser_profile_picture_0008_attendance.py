1cbf617af9b4e00bf9e3ad46c41ab47c1271035e
AbbasWork7 <syedabbas.keykoders@gmail.com>
2025-06-27 17:48:06 +0530

    new chnages

--- Backend/vtraco/users/migrations/0009_merge_0007_customuser_profile_picture_0008_attendance.py
+++ Backend/vtraco/users/migrations/0009_merge_0007_customuser_profile_picture_0008_attendance.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0007_customuser_profile_picture'),
+        # ('users', '0007_customuser_profile_picture'),
         ('users', '0008_attendance'),
     ]
 
